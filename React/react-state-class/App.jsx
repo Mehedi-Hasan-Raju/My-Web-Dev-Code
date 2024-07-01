48382a11c75ec4fa5ffc1dc903233905608bf197
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-01 10:30:27 +0600

    Update App.jsx
    App.jsx created

--- React/react-state-class/App.jsx
+++ React/react-state-class/App.jsx
@@ -1,1 +1,16 @@
+import './App.css';
+import LikeButton from './likeButton';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>state in react</h1>
+      <LikeButton/>
+    </>
+  );
+}
+
+export default App;
 
