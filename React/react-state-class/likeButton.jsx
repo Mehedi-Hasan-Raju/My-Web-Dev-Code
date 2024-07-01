6ba779abdbee22255094971ca926d26e5130db82
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-01 11:06:44 +0600

    Update likeButton.jsx
    like button with clicks

--- React/react-state-class/likeButton.jsx
+++ React/react-state-class/likeButton.jsx
@@ -2,13 +2,16 @@ import { useState } from "react"
 
 export default function LikeButton() {
 let [isLiked, setIsLiked] = useState(false)
+let [clicks, setclicks] = useState(0);
 
    let toggleLike = () => {
           setIsLiked(!isLiked);
+          setclicks(clicks+1);
     };
     let likeStyle = {color: "red"}
     return (
         <div>
+            <p>clicks ={clicks} </p>
             <p onClick={toggleLike}>
                 {isLiked ? (<i className="fa-solid fa-heart"  style={likeStyle}></i>
                 ) :(
