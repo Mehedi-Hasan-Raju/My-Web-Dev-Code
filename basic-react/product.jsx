bbddaa90e9beaf14874bf061ec3fb909482ce4e8
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-02 09:46:45 +0600

    Update product.jsx
    for creating product page

--- basic-react/product.jsx
+++ basic-react/product.jsx
@@ -1,1 +1,23 @@
+import "./product.css";
+import Price  from "./Price.jsx";
 
+function Product({title,price,features,idx}) {
+    let oldPrices = ["12,495","11,900","1,599","599"];
+    let newPrices = ["9,999","8,999","900","400"];
+    let Description = [["8000 DPI","5 programmable buttons"],
+        ["intuitive surface","designes for ipad pro"],
+        ["designed for iPad pro","intuitive surface"],
+        ["wirless","optical oreantation"]];
+    return (
+        <div className="Product">
+            <h4>{title}</h4>
+            <p>{Description[idx][0]}</p>
+            <p>{Description[idx][1]}</p>
+            <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
+        </div>
+    );
+}
+
+
+
+export default Product;
