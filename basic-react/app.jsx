e7c248a587bf3013f7c61011e5c2d458cb69df66
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-02 09:50:12 +0600

    Create app.jsx

---
+++ basic-react/app.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Button from "./Button.jsx";
+import ProductTab from "./ProductTab.jsx"; 
+import Form from "./Form.jsx";
+
+           
+
+function App() {
+  return (
+  <> 
+    
+      <Form/>
+  </> 
+  );
+}
+
+export default App;
