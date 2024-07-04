558d6b35dfc67e72af3d51617fe9184f481442f7
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-04 20:24:16 +0600

    Create todoliston singel array element update.jsx
    here we can update single li element

---
+++ React/react-state-class/todoliston singel array element update.jsx
@@ -0,0 +1,89 @@
+
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+export default function TodoList() {
+    let [todo, setTodo] = useState([{task: "sample task", id: uuidv4() }]);
+    let [newTodo, setNewTodo] = useState("");
+
+let addNewTask = () => {
+ setTodo ((prevTodo) => {
+     return ([...prevTodo, {task: newTodo, id: uuidv4() } ]);
+ });
+ setNewTodo("");
+};
+let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+};
+
+let deletTodo = (id) => {
+    setTodo(todo.filter((todos) => todos.id !=id));
+};
+
+let upperCaseAll = () =>{
+    setTodo( (prevTodo)=>( 
+    prevTodo.map((todo) => {
+        return{
+            ...todo,
+            task: todo.task.toUpperCase()
+        };
+    })));
+    
+};
+
+let lowerCaseAll= ()=> {
+    setTodo( (prevTodo) =>(
+        prevTodo.map((todo) => {
+        return{
+            ...todo,
+            task: todo.task.toLowerCase()
+        };
+    })));
+    
+};
+
+let upperCaseOne = (id) =>{
+    setTodo( (prevTodo)=> 
+    prevTodo.map((todo) => {
+        if (todo.id == id) {
+            return{
+                ...todo,
+                task: todo.task.toUpperCase(),
+            };
+        }else{
+            return todo;
+        }
+       
+    }));
+    
+};
+
+
+    return (
+        <div>
+            <input placeholder="add a task" value={newTodo}  onChange={updateTodoValue}></input><br></br>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <h4>Task To do</h4>
+            <hr></hr>
+            <ul>
+                {
+                  todo.map((todos) => (
+                     <li key={todos.id}>
+                     <span>{todos.task}</span>   
+                     &nbsp; &nbsp; &nbsp; &nbsp;
+                     <button onClick  = { () => deletTodo(todos.id)}>Delete</button>
+                     <button onClick={() => upperCaseOne(todos.id)}>UpperCase One</button>
+                     </li>
+    ))}
+            </ul>
+            <button onClick={upperCaseAll}>UpperCase All</button>
+            &nbsp;
+            <button onClick={lowerCaseAll}>LowerCase All</button>
+            <br></br>
+        </div>
+    )
+}
