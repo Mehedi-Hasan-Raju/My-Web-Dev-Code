48995cfd88bcf1d8ba55d0214cf01f010dc86ff6
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-06 13:30:20 +0600

    Update app.jsx

--- React/react-state-class/proper way to write react/app.jsx
+++ React/react-state-class/proper way to write react/app.jsx
@@ -1,1 +1,21 @@
+import './App.css';
+import Counter from './Counter';
+import LikeButton from './likeButton';
+import Lottery from './Lottery';
+import LudoBoard from './LudoBoard';
+import TodoList from './TodoList';
+import TicketNum from './TicketNum';
+import Ticket from './Ticket';
+
+
+function App() {
+
+  return (
+    <>
+      <Lottery n={5} WinningSum={125}/>
+    </>
+  );
+}
+
+export default App;
 
