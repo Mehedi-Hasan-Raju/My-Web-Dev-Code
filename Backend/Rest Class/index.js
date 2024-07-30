edf9a0cadddcd2d98e2cf8af3b3d7cd27068c37d
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2024-07-30 16:22:47 +0600

    Update index.js

--- Backend/Rest Class/index.js
+++ Backend/Rest Class/index.js
@@ -1,1 +1,57 @@
+const express = require("express");
+const app = express();
+const port =7777;
+const path = require("path");
 
+const { v4: uuidv4 } = require('uuid');
+
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        id:uuidv4(),
+        username : "apnacollege",
+    content : "I love coading",
+    },
+    {
+        id: uuidv4(),
+        username: "rahul kumar",
+        content: "Hard working is important to achive success",
+    },
+    {
+        id:uuidv4(),
+        username:"mehedi",
+        content:"i got slected for my 1st internship",
+    },
+];
+
+
+app.get("/posts" ,(req, res) => {
+   res.render("index.ejs", {posts});
+});
+
+app.get("/posts/new" ,(req, res) => {
+    res.render("form.ejs");
+ });
+
+ app.post("/posts", (req , res) => {
+    let {username , content} = req.body;
+    let id = uuidv4();
+    posts.push({id, username, content});
+    res.send("post req working");
+ })
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post} );
+})
+
+app.listen(port, () => {
+    console.log("listning to port: 7777");
+});
